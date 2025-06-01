239d4654ecef3ad4338b3ea128eb7db736f02a8e
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-01 09:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	  let d1 = date1.split("-").map(i => Number(i));
+  
+  let d2 = date2.split("-").map(i => Number(i));
+    return d2[2]-d1[2];
 };
 
 // Do not change the code below.
